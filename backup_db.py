265be3fb1aaf34a925f975a2003de1fc2a0ac6bc
Mikhai Baranov <mikhail.baranov@gmail.com>
2013-06-20 21:40:02 +0000

    Simple mysql backup script

---
+++ backup_db.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import time
+import argparse
+import gzip
+import subprocess
+from datetime import datetime
+
+backups_ttl = 14 * 86400
+
+parser = argparse.ArgumentParser(description='MySQL backup utility')
+
+parser.add_argument('-u', '--username', help='database user name')
+parser.add_argument('-p', '--password', help='database password')
+parser.add_argument('-d', '--dir', help='backups dir')
+parser.add_argument('database', help='database name')
+
+args = parser.parse_args()
+
+mysql_cmd = ['mysqldump', '--user=' + args.username, '--password=' + args.password, args.database] 
+mysql_proc = subprocess.Popen(mysql_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+mysql_out = mysql_proc.communicate()[0]
+
+dt = datetime.today().strftime('%Y_%m_%d')
+
+fname = '{}/{}__{}.gz'.format(args.dir, args.database, dt)
+
+f = gzip.open(fname, 'wb')
+f.write(mysql_out)
+f.close()
+
+now = time.time()
+
+for file in os.listdir(args.dir):
+	fpath = os.path.join(args.dir, file)
+	if os.stat(fpath).st_mtime < now - backups_ttl:
+		os.remove(fpath)
+
