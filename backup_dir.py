f2b6f69cf3154ad8274ca4a17455cf9ebc9cad44
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-08-02 01:31:44 +0400

    Написал скрипт копирования каталогов

---
+++ backup_dir.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Скрипт резервного копирования каталога
+
+Файлы хранятся 14 дней
+Ключи
+-d --dir        - каталог с резервными копиями
+-n --filename   - имя файла резервной копии
+"""
+
+import os
+import time
+import argparse
+import tarfile
+from datetime import datetime
+
+backups_ttl = 14 * 86400
+
+parser = argparse.ArgumentParser(description='Directory backup utility')
+
+parser.add_argument('-d', '--dir', help='backups dir')
+parser.add_argument('-n', '--filename', help='backup file name')
+parser.add_argument('target', help='target directory')
+
+args = parser.parse_args()
+
+dt = datetime.today().strftime('%Y_%m_%d')
+
+fname = '{}/{}__{}.tar.gz'.format(args.dir, args.filename, dt)
+now = time.time()
+
+try:
+    archive = tarfile.open(fname, "w:gz")
+    archive.add(args.target)
+    archive.close()
+except tarfile.TarError as error:
+    pass
+
+for fname in os.listdir(args.dir):
+    fpath = os.path.join(args.dir, fname)
+    if os.stat(fpath).st_mtime < now - backups_ttl:
+        os.remove(fpath)
