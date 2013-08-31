c26ceb2b04ff48210b01d5495135bfebf657493d
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-08-31 23:03:38 +0400

    Функция отправки самого свежего файла на удалённый сервер

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import os
 from time import time
+from datetime import date
+import easywebdav
 
 
 def cleanDir(path, ttl):
@@ -10,3 +12,23 @@ def cleanDir(path, ttl):
         fpath = os.path.join(path, fname)
         if os.stat(fpath).st_mtime < now - ttl:
             os.remove(fpath)
+
+
+def sortFile(dirname, reverse=True):
+    dirlist = os.listdir(dirname)
+    return sorted(dirlist, key=lambda x: os.lstat(x).st_mtime, reverse=reverse)
+
+
+def sendToRemote(dirname, url, baseurl=None, username="anonimous", password="anonimous", scheduled="6"):
+    today = date.today()
+    if str(today.weekday()) not in scheduled:
+        return
+
+    fname = sortFile(dirname)[0]
+    client = easywebdav.connect(url, username=username, password=password)
+    if baseurl is not None:
+        client.baseurl = baseurl
+    try:
+        client.upload(fname, fname)
+    except easywebdav.WebdavException as e:
+        pass
